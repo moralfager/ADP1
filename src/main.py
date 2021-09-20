3bc9e0eae90da6f74c3e6fa38f482c6009ac2f92
moralfager <74453899+moralfager@users.noreply.github.com>
2021-09-20 09:23:48 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ path = "example.json"
 with open("example.json", "w") as json_file:
     json.dump(data1, json_file, indent=2)
 count = 0
+print("write number")
+terminal = int(input())
 data1.sort(key=operator.itemgetter('market_cap'), reverse=True)
-for x in data1:
+for x in range(count,terminal):
     print(str(data1[count]["id"])+" "+str(data1[count]["market_cap"]))
     count = count + 1
